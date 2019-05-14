d2a458d99ecf5f7001bae05e0c8626368c4ee96a
Sahil Khosla <sahilkhosla@gmail.com>
2019-05-14 07:16:22 -0400

    wiring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import TextInput from './lib/TextInput/';
+
+const App = () => (
+    <div style={{ width: 640, margin: "15px auto" }}>
+        <h1>Hello Bubble Tea UI</h1>
+        <TextInput label="Email Address" placeholder="name@example.com" />
+    </div>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
